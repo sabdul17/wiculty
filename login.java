8b195949e0402ed91b2e026d28cdc778c5c3268c
shaik <shaikdr7@gmail.com>
2022-01-16 22:55:47 +0530

    changes

---
+++ login.java
@@ -0,0 +1,13 @@
+public class Login 
+{
+ 
+   public static void main(String[] args) 
+   {
+
+      int i;
+      for(i=0;i<=30;i++)
+      {
+      System.out.println("Numbers: "+i);
+      }
+   }
+}
